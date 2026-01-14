c253179fecf75187d6c46583da714ab6cdb7a4e2
Made Subali <os.madesubali@gmail.com>
2026-01-14 13:13:13 +0800

    Pembuatan fitur upload file dengan multer.

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require("express");
+const multer = require("multer");
+const path = require("path");
 const sequelize = require("./utils/db.util");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads");
+  },
+  filename: function (req, file, cb) {
+    const extension = path.extname(file.originalname);
+    const uniqueSuffix = Date.now() + extension;
+
+    cb(null, uniqueSuffix);
+  },
+});
+
+const upload = multer({ storage: storage });
 
 const productRoute = require("./routes/product.route");
 
 const app = express();
 
 app.use(express.json());
+app.use("/public", express.static("uploads"));
 
 app.use("/products", productRoute);
 
@@ -13,6 +30,11 @@ app.get("/", (req, res) => {
   const pesan = { message: "API MK Backend" };
 
   res.status(200).json(pesan);
+});
+
+app.post("/profile", upload.single("foto"), function (req, res, next) {
+  const { id } = req.body;
+  res.status(200).json({ id, file: req.file.filename });
 });
 
 try {
